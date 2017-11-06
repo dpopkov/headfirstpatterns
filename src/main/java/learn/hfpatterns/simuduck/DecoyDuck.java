940d9a30b3793e9f89358ae48900e44cb54e6c88
dpopkov <pkvdenis@gmail.com>
2017-11-06 12:28:21 +0300

    Add DecoyDuck

---
+++ src/main/java/learn/hfpatterns/simuduck/DecoyDuck.java
@@ -0,0 +1,8 @@
+package learn.hfpatterns.simuduck;
+
+public class DecoyDuck extends Duck {
+    @Override
+    public void display() {
+        System.out.println("\nThis duck looks like DecoyDuck");
+    }
+}
--- src/main/java/learn/hfpatterns/simuduck/RubberDuck.java
+++ src/main/java/learn/hfpatterns/simuduck/RubberDuck.java
@@ -2,11 +2,6 @@ package learn.hfpatterns.simuduck;
 
 public class RubberDuck extends Duck {
     @Override
-    public void quack() {
-        System.out.println("squeak squeak squeak");
-    }
-
-    @Override
     public void display() {
         System.out.println("\nLooks like a RubberDuck");
     }
