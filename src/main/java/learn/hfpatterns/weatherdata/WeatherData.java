4d2ea83ff2d24c43ea03867121b6462578d170d0
dpopkov <pkvdenis@gmail.com>
2017-11-09 21:48:44 +0300

    Initial weather data.

---
+++ src/main/java/learn/hfpatterns/weatherdata/WeatherData.java
@@ -0,0 +1,27 @@
+package learn.hfpatterns.weatherdata;
+
+public class WeatherData {
+    public double getTemperature() {
+        return 0.0;
+    }
+
+    public double getHumidity() {
+        return 0.0;
+    }
+
+    public double getPressure() {
+        return 0.0;
+    }
+
+    /**
+     * This method is called whenever the weather
+     * measurements have been updated.
+     */
+    public void measurementsChanged() {
+        /*
+        Our job is to implement measurementsChanged()
+        so that it updates the three displays for current
+        conditions, weather stats, and forecase.
+         */
+    }
+}
