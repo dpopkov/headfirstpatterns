63921eb467a4995e36c1c2556b2fde9da310b159
dpopkov <pkvdenis@gmail.com>
2017-11-12 18:19:45 +0300

    Add PizzaTestDrive main class, remote SimplePizzaFactory.

---
+++ src/main/java/learn/hfpatterns/pizzashop/PizzaTestDrive.java
@@ -0,0 +1,17 @@
+package learn.hfpatterns.pizzashop;
+
+import learn.hfpatterns.pizzashop.chicago.ChicagoStylePizzaStore;
+import learn.hfpatterns.pizzashop.ny.NYStylePizzaStore;
+
+public class PizzaTestDrive {
+    public static void main(String[] args) {
+        PizzaStore nyStore = new NYStylePizzaStore();
+        PizzaStore chicagoStore = new ChicagoStylePizzaStore();
+
+        Pizza pizza = nyStore.orderPizza("cheese");
+        System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza("cheese");
+        System.out.println("Joel ordered a " + pizza.getName() + "\n");
+    }
+}
